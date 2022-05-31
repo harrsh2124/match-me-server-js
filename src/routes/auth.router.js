4bf60a4c90339df1e35fea53da52b98d1f5e42d2
Harrsh Patel <me.harrsh.patel@gmail.com>
2022-05-31 20:21:24 +0530

    Tweaks

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const schemaValidator = require("../config/schemaValidator");
+const schemaValidator = require("../utils/schemaValidator");
 const ConfirmUserController = require("../controllers/auth/ConfirmUser.controller");
 const SignInController = require("../controllers/auth/SignIn.controller");
 const SignUpController = require("../controllers/auth/SignUp.controller");
