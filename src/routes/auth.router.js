ea94d9672d07564af08be3d7fdf221a686675d2e
Harrsh Patel <me.harrsh.patel@gmail.com>
2022-06-01 22:10:10 +0530

    Fix

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const schemaValidator = require("../utils/schemaValidator");
-const ConfirmUserController = require("../controllers/auth/ConfirmUser.controller");
+// const ConfirmUserController = require("../controllers/auth/ConfirmUser.controller");
 const SignInController = require("../controllers/auth/SignIn.controller");
 const SignUpController = require("../controllers/auth/SignUp.controller");
 const signUpSchema = require("../schemas/auth/signUp.schema");
@@ -10,6 +10,6 @@ const AuthRoute = express.Router();
 
 AuthRoute.post("/signup", schemaValidator(signUpSchema), SignUpController);
 AuthRoute.post("/signin", schemaValidator(signInSchema), SignInController);
-AuthRoute.post("/verify/:userId/:verificationToken", ConfirmUserController);
+// AuthRoute.post("/verify/:userId/:verificationToken", ConfirmUserController);
 
 module.exports = AuthRoute;
