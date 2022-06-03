252a7a81313c02babf99a6d5c318bfc85602af4f
Harrsh Patel <me.harrsh.patel@gmail.com>
2022-06-04 01:08:18 +0530

    Tweaks

--- app.js
+++ app.js
@@ -6,11 +6,6 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(
-    express.urlencoded({
-        extended: false,
-    })
-);
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
